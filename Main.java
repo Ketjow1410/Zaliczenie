ae92134ce2b9f8c9e97e2313436c9630e3d4b124
ketjow <100692239+Ketjow1410@users.noreply.github.com>
2022-06-21 12:19:56 +0000

    zaliczenie

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] tab = { 54, 2, 30, 35, 65, 34, 5, 8, 23, 65, 4, 5, 6, 3, 59, 55, 3, 2, 54, 5, 5 };
+        int dlugosc = tab.length;
+        int min = tab[0];
+        for (int i = 0; i < dlugosc; i++) {
+            if (tab[i] < min)
+                min = tab[i];
+        }
+        System.out.println(min);
+    }
 }
